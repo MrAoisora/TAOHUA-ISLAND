a3da45df475c127df007ad58657991a7c947b4e9
Aoisora <428365462@qq.com>
2022-07-09 23:56:42 +0800

    添加日志输出，目前日志输出为硬编码

--- taohua-server/src/main/java/com/taohua/controller/LoginController.java
+++ taohua-server/src/main/java/com/taohua/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.taohua.controller;
 
-import org.springframework.http.ResponseEntity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/login")
 public class LoginController {
+    public static final Logger log = LoggerFactory.getLogger(LoginController.class);
     @PostMapping("/loginVerification")
     public void login(@RequestBody Map<String,String> paramMap){
-        System.out.println("============执行登录========");
+        log.info("========执行登录方法info===========");
+        log.warn("=======执行登录方法warn");
+        log.error("====执行登录方法error=====");
+        log.debug("====执行登录方法debuger====");
     }
 }
